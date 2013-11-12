59e3dabffa938794b38b48b49627f260364e9154
Lorin Klugman <lorin@public-action.org>
2013-11-12 14:32:08 -0600

    Add unit tests and new functions

--- project.clj
+++ project.clj
@@ -1,14 +1,30 @@
-(defproject book-review "Rel 1.0"
-  :description "Book Review Site "
+(defproject book-review "V 2.0"
+  :description "Book Club Site "
   :url "http://public-action.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-cljsbuild "0.3.2"]]
-  :cljsbuild {
-    :builds [{:source-paths ["src/cljs"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1859"]
+                 [com.cemerick/clojurescript.test "0.2.1"]
+                 ]
+  :plugins [[lein-cljsbuild "0.3.2"] [com.cemerick/clojurescript.test "0.2.1"][marginalia "0.7.1"] [lein-marginalia "0.7.1"]]
+  :cljsbuild
+  {
+    :builds [
+             {:source-paths ["src/cljs"]
               :compiler {:output-to "resources/public/books_cljs.js"
-                         :libs ["closure/library/third_party/closure"]
                          :optimizations :advanced
-                         :pretty-print true}}]}
+                         :pretty-print true}}
+             {:source-paths ["src/cljs" "test" ]
+              :compiler {:output-to "target/cljs/wstestable.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}
+           ]
+          :test-commands {
+                    "phantom-ws" ["phantomjs" :runner
+                                          "window.literal_js_was_evaluated=true"
+                                          "target/cljs/wstestable.js"
+                                          "test/cemerick/cljs/test/extra_test_command_file.js"]
+          } 
+  }
 )
---
+++ test/book_review/books_test.cljs
@@ -0,0 +1,61 @@
+(ns book-review.books_test
+  (:require-macros [cemerick.cljs.test :refer (is deftest with-test run-tests testing)])
+  (:require [cemerick.cljs.test :as t]
+            [book-review.books :as p]
+  )
+)
+
+(deftest add-authors
+  (testing "add authors"
+    (p/addAuthor 0 "Mark" "Twain")
+    (p/addAuthor 1 "Sam" "Shepard")
+    (let [ 
+          cnt (count  (deref p/AuthorList))
+    ]
+      (is (= cnt 2)) 
+    )
+  )  
+)
+
+
+(deftest get-author
+  (testing "add authors"
+    (p/addAuthor 2 "Ann" "Sanders")
+    (let [ 
+          cnt (count  (deref p/AuthorList))
+          ann (p/get-list-item-by-id p/AuthorList 2)
+    ]
+      (is (= cnt 3)) 
+      (is (= (:first_name ann) "Ann"))
+    )
+  )  
+)
+
+(deftest delete-author 
+ (testing "delete an author"
+    (p/remove-item p/AuthorList 2)
+    (is (= (count (deref p/AuthorList)) 2))
+  ) 
+)  
+
+(deftest add-author-with-json 
+  (testing "adding an author with a json object"
+    (let
+      [ jsonObj "{\"id\": 4, \"first_name\":\"Mary\",\"last_name\": \"Wallace\"}"]  
+      (p/addAuthor jsonObj)
+      (is (= (count (deref p/AuthorList)) 3))
+    )
+  ) 
+)
+
+(deftest modify-author
+  (testing "modify author"
+       (p/modifyAuthor (p/Author. 1 "Sammy" "Foo"))
+       (is
+           (=
+             (:first_name  (p/get-list-item-by-id p/AuthorList 1)) 
+             "Sammy"
+          )
+         )
+   )  
+  ) 
