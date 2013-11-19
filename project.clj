146d2ea2de9b00ffd04ea3fd09dc702a793bb66f
Lorin Klugman <lorin@public-action.org>
2013-11-19 16:28:54 -0600

    Added cemerick clojurescript test library

--- project.clj
+++ project.clj
@@ -7,18 +7,19 @@
                  [org.clojure/clojurescript "0.0-1859"]
                  [com.cemerick/clojurescript.test "0.2.1"]
                  ]
-  :plugins [[lein-cljsbuild "0.3.2"] [com.cemerick/clojurescript.test "0.2.1"][marginalia "0.7.1"] [lein-marginalia "0.7.1"]]
+  :plugins [[lein-cljsbuild "0.3.3"] [com.cemerick/clojurescript.test "0.2.1"][marginalia "0.7.1"] [lein-marginalia "0.7.1"]]
   :cljsbuild
   {
     :builds [
-             {:source-paths ["src/cljs"]
-              :compiler {:output-to "resources/public/books_cljs.js"
-                         :optimizations :advanced
-                         :pretty-print true}}
-             {:source-paths ["src/cljs" "test" ]
-              :compiler {:output-to "target/cljs/wstestable.js"
+              { :source-paths ["src/cljs" "test" ]
+                :compiler {:output-to "target/cljs/wstestable.js"
                          :optimizations :whitespace
                          :pretty-print true}}
+              {:id "prod" :source-paths ["src/cljs"]
+                :compiler {:output-to "target/cljs/books_cljs.js"
+                         :optimizations :whitespace
+                         :pretty-print true}}
+
            ]
           :test-commands {
                     "phantom-ws" ["phantomjs" :runner
